08a0f0faeaced83ff4a284cfe913df6fe6da6c15
Abuzarov Maxim <maxim.abuzarov@gmail.com>
2022-04-01 11:33:42 +0700

    fix: _document title error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import Head from 'next/head'
 import type {AppProps} from 'next/app'
 import '/styles/app.css'
 
 const PortfolioApp = ({Component, pageProps}: AppProps): JSX.Element => {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Abuzarov Maxim</title>
+				<meta name="description" content="Portfolio Website"/>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default PortfolioApp
